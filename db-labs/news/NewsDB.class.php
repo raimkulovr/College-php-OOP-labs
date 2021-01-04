0dac6336ba0c6fb43bc6e8e8b14c5b2458cee465
raimkulovr <58400439+raimkulovr@users.noreply.github.com>
2021-01-04 16:24:50 +0600

    lab1

--- db-labs/news/NewsDB.class.php
+++ db-labs/news/NewsDB.class.php
@@ -1,1 +1,29 @@
 <?php
+    spl_autoload_register(function ($class_name) {
+        include $class_name . '.class.php';
+    });
+    class NewsDB implements INewsDB{
+        const DB_NAME = 'news.db';
+        protected $_db;
+        
+        
+        function __construct()
+        {
+            $_db = new PDO("sqlite:".NewsDB::DB_NAME);
+        }
+        function saveNews($title, $category, $description, $source){
+
+        }
+        function getNews(){
+
+        }
+        function showNews($id){
+
+        }
+        function __destruct()
+        {
+            $_db = NULL;
+        }
+    }
+
+    $news = new NewsDB;
